3ce859d628df4c32dd76e7f7e3aa69fa749e4535
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2024-05-15 20:24:38 +0300

    feat(types): types for registerUser, user and user store interface

---
+++ types/types.go
@@ -0,0 +1,27 @@
+package types
+
+import (
+	"time"
+)
+
+type RegisterUserPayload struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	GetUserById(id string) (*User, error)
+	CreateUser(User) error
+}
