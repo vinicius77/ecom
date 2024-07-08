ef08961454e7d6f372d3c0fe760d488f61611c3c
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2024-07-08 20:55:05 +0300

    feat(env-variables): helper to getEnvAsInt and new env properties realted to JWT secret and expiration token date

--- config/env.go
+++ config/env.go
@@ -2,17 +2,21 @@ package config
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
 	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	PublicHost string
-	Port       string
-	DBUser     string
-	DBPassword string
-	DBAddress  string
-	DBName     string
+	PublicHost             string
+	Port                   string
+	DBUser                 string
+	DBPassword             string
+	DBAddress              string
+	DBName                 string
+	JWTExpirationInSeconds int64
+	JWTSecret              string
 }
 
 const PUBLIC_HOST = "PUBLIC_HOST"
@@ -23,6 +27,8 @@ const DB_ADDRESS = "DB_ADDRESS"
 const DB_HOST = "DB_HOST"
 const DB_PORT = "DB_PORT"
 const DB_NAME = "DB_NAME"
+const JWT_EXPIRATION_IN_SECONDS = "JWT_EXPIRATION_IN_SECONDS"
+const JWT_SECRET = "JWT_SECRET"
 
 var Envs = initConfig()
 
@@ -34,18 +40,34 @@ func initConfig() Config {
 	dbAddress := fmt.Sprintf("%s:%s", dbHost, port)
 
 	return Config{
-		PublicHost: getEnv(PUBLIC_HOST, "http://localhost"),
-		Port:       getEnv(PORT, "8080"),
-		DBUser:     getEnv(DB_USER, "root"),
-		DBPassword: getEnv(DB_PASSWORD, "root"),
-		DBAddress:  getEnv(DB_ADDRESS, dbAddress),
-		DBName:     getEnv(DB_NAME, "ecom"),
+		PublicHost:             getEnv(PUBLIC_HOST, "http://localhost"),
+		Port:                   getEnv(PORT, "8080"),
+		DBUser:                 getEnv(DB_USER, "root"),
+		DBPassword:             getEnv(DB_PASSWORD, "root"),
+		DBAddress:              getEnv(DB_ADDRESS, dbAddress),
+		DBName:                 getEnv(DB_NAME, "ecom"),
+		JWTExpirationInSeconds: getEnvAsInt(JWT_EXPIRATION_IN_SECONDS, 3600*24*7*52), // 52 Weeks (1 year)
+		JWTSecret:              getEnv(JWT_SECRET, "ecom-secret"),
 	}
 }
 
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
+	}
+
+	return fallback
+}
+
+func getEnvAsInt(key string, fallback int64) int64 {
+	if value, ok := os.LookupEnv(key); ok {
+		i, err := strconv.ParseInt(value, 10, 64)
+
+		if err != nil {
+			return fallback
+		}
+
+		return i
 	}
 
 	return fallback
