fc9c02be4ba59143628784562036ea59372a26be
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2024-07-18 20:16:49 +0300

    feat(products-service): initialize product service and route handler

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/vinicius77/ecom/cmd/service/product"
 	"github.com/vinicius77/ecom/cmd/service/user"
 )
 
@@ -25,9 +26,14 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
 	userStore := user.NewStore(s.db)
 	userHandler := user.NewHandler(userStore)
 	userHandler.RegisterRoutes(subrouter)
+
+	productStore := product.NewStore(s.db)
+	productHandler := product.NewHandler(productStore)
+	productHandler.RegisterRoutes(subrouter)
 
 	log.Println("Listening at", s.addr)
 
