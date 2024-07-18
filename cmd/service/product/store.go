55f31cff1bb640f7e909c168d487020ec56a8932
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2024-07-18 20:19:02 +0300

    feat(product-store): initial product store

---
+++ cmd/service/product/store.go
@@ -0,0 +1,56 @@
+package product
+
+import (
+	"database/sql"
+	"github.com/vinicius77/ecom/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) GetProducts() ([]types.Product, error) {
+	rows, err := s.db.Query("SELECT * FROM products")
+
+	if err != nil {
+		return nil, err
+	}
+
+	products := make([]types.Product, 0)
+
+	for rows.Next() {
+		product, err := ScanRowsIntoProducts(rows)
+		if err != nil {
+			return nil, err
+		}
+
+		products = append(products, *product)
+	}
+
+	return products, nil
+
+}
+
+func ScanRowsIntoProducts(rows *sql.Rows) (*types.Product, error) {
+	product := new(types.Product)
+
+	err := rows.Scan(
+		&product.ID,
+		&product.Name,
+		&product.Description,
+		&product.Image,
+		&product.Price,
+		&product.Quantity,
+		&product.CreatedAt)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+
+}
