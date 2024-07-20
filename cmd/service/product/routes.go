b62cd36a939a3ba82ae48fd8c3f80e0af423cebe
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2024-07-20 20:59:44 +0300

    feat(product-routes): create product router handler

--- cmd/service/product/routes.go
+++ cmd/service/product/routes.go
@@ -1,10 +1,13 @@
 package product
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 	"github.com/vinicius77/ecom/types"
 	"github.com/vinicius77/ecom/utils"
-	"net/http"
 )
 
 type Handler struct {
@@ -16,7 +19,37 @@ func NewHandler(store types.ProductStore) *Handler {
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
-	router.HandleFunc("/products", h.GetProducts).Methods("GET")
+	router.HandleFunc("/products", h.GetProducts).Methods(http.MethodGet)
+	router.HandleFunc("/product", h.HandleCreateProduct).Methods(http.MethodPost)
+}
+
+func (h *Handler) HandleCreateProduct(w http.ResponseWriter, r *http.Request) {
+	var payload types.CreateProductPayload
+
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validator.Struct(payload); err != nil {
+		error := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload %v", error))
+		return
+	}
+
+	err := h.store.CreateProduct(types.Product{
+		Name:        payload.Name,
+		Description: payload.Description,
+		Image:       payload.Image,
+		Price:       payload.Price,
+		Quantity:    payload.Quantity,
+	})
+
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, payload)
 }
 
 func (h *Handler) GetProducts(w http.ResponseWriter, r *http.Request) {
