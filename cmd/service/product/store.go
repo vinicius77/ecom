98c2af4691469200af06659b5a2eb2909709b301
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2024-08-07 20:37:33 +0300

    feat(product-store): implement updateProduct and getProductByID methods

--- cmd/service/product/store.go
+++ cmd/service/product/store.go
@@ -2,6 +2,9 @@ package product
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
+
 	"github.com/vinicius77/ecom/types"
 )
 
@@ -63,6 +66,52 @@ func (s *Store) CreateProduct(product types.Product) error {
 		product.Image,
 		product.Price,
 		product.Quantity)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) GetProductByID(productIDS []int) ([]types.Product, error) {
+	placeholders := strings.Repeat(",?", len(productIDS)-1)
+	query := fmt.Sprintf("SELECT * FROM products WHERE id IN (?%s)", placeholders)
+
+	// Convert ProductIDS into []interface{ }
+	args := make([]interface{}, len(productIDS))
+	for i, v := range productIDS {
+		args[i] = v
+	}
+
+	rows, err := s.db.Query(query, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	products := []types.Product{}
+	for rows.Next() {
+		p, err := ScanRowsIntoProducts(rows)
+		if err != nil {
+			return nil, err
+		}
+
+		products = append(products, *p)
+	}
+
+	return products, nil
+}
+
+func (s *Store) UpdateProduct(product types.Product) error {
+	_, err := s.db.Exec("Update products SET name = ?, price = ?, image = ?, description = ?, price = ?, quantity = ? WHERE id = ?",
+		product.Name,
+		product.Price,
+		product.Image,
+		product.Description,
+		product.Price,
+		product.Quantity,
+		product.ID,
+	)
 
 	if err != nil {
 		return err
