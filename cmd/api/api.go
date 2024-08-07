208dd98be5b6003b806dfe328255f6edae5055ba
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2024-08-07 21:14:00 +0300

    feat(order-and-cart): implement cart route handler and instantiate order store

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/vinicius77/ecom/cmd/service/cart"
+	"github.com/vinicius77/ecom/cmd/service/order"
 	"github.com/vinicius77/ecom/cmd/service/product"
 	"github.com/vinicius77/ecom/cmd/service/user"
 )
@@ -34,6 +36,10 @@ func (s *APIServer) Run() error {
 	productStore := product.NewStore(s.db)
 	productHandler := product.NewHandler(productStore)
 	productHandler.RegisterRoutes(subrouter)
+
+	orderStore := order.NewStore(s.db)
+	cartHandler := cart.NewHandler(orderStore, productStore, userStore)
+	cartHandler.RegisterRoutes((subrouter))
 
 	log.Println("Listening at", s.addr)
 
